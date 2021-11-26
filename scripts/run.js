9d7079a5a089645cfb714a39ac022afd2dee22d8
0xMousy <0xmousy@gmail.com>
2021-11-25 17:12:54 -0800

    Added character data

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,13 @@
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
-    const gameContract = await gameContractFactory.deploy();
+    const gameContract = await gameContractFactory.deploy(
+      ["Goku", "Vegeta", "Gohan"],       // Names
+      ["https://i.imgur.com/ToBebz9b.jpg", // Images
+      "https://i.imgur.com/aRy02l9b.jpg", 
+      "https://i.imgur.com/cmVNaT0b.jpg"],
+      [100, 500, 1337],                    // HP values
+      [1337, 100, 50]                      // Attack damage values
+    );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
   };
