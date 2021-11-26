2ad3c91a851d921222c4b6ff48b6a0fb9987fc4f
0xMousy <0xmousy@gmail.com>
2021-11-26 02:03:37 -0800

    Updated hardhat config to accept mainnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -23,8 +23,13 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     rinkeby: {
-      url: ALCHEMY_API_URL,
-      accounts: [RINKEBY_PRIVATE_KEY],
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
